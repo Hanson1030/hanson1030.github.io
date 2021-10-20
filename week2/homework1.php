d104b552b6bccea8432d79e2105b770843f4571a
Mak Hon Sang <makhonsang1030@e.newera.edu.my>
2021-10-20 17:13:40 +0800

    added week2 homework 1

---
+++ week2/homework1.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous" />
+    <title>
+        Homework 1
+    </title>
+</head>
+
+<body>
+    <form class="text-center">
+        <div class="text-center">
+            <h1>What is your birthday? Please fill in !</h1>
+
+            <?php
+
+            echo '<select class="bg-primary fs-2 rounded" id="day" name="day">' . "\n";
+            echo "<option class='bg-white' selected>Day</option>" . "\n";
+
+            for ($i_day = 1; $i_day <= 31; $i_day++) {
+                echo "<option class='bg-white'>" . $i_day . "</option>" . "\n";
+            }
+            echo '</select>' . "\n";
+            ?>
+
+            <?php
+
+            echo '<select class="bg-secondary fs-2 rounded" id="month" name="month">' . "\n";
+            echo "<option class='bg-white' selected>Month</option>" . "\n";
+
+            for ($i_month = 1; $i_month <= 12; $i_month++) {
+                echo "<option class='bg-white'>" . $i_month . "</option>" . "\n";
+            }
+            echo '</select>' . "\n";
+            ?>
+
+            <?php
+            $staring_year  = 1900;
+            $curr_year = date('Y');
+
+            echo '<select class="bg-warning fs-2 rounded" id="year" name="year">' . "\n";
+            echo "<option class='bg-white' selected>Year</option>" . "\n";
+
+            for ($year = $staring_year; $year <= $curr_year; $year++) {
+                echo "<option class='bg-white'>" . $year . "</option>" . "\n";
+            }
+            echo '</select>' . "\n";
+            ?>
+
+
+
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-kQtW33rZJAHjgefvhyyzcGF3C5TFyBQBA13V1RKPf4uH+bwyzQxZ6CmMZHmNBEfJ" crossorigin="anonymous"></script>
+        </div>
+
+        <div class="m-3">
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Submit your birthday</button>
+            <!-- Modal -->
+            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Thank you for joining our servey! ^^
+                        </div>
+                        <div class="modal-footer">
+                            <button type="submit" class="btn btn-secondary" data-bs-dismiss="modal" onClick="window.location.reload();">
+                                Close
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</body>
+
+</html>
