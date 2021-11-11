b4734cf61f879b4a2aa63619fdf71a8ff52d0d4e
Mak Hon Sang <makhonsang1030@e.newera.edu.my>
2021-11-11 12:14:41 +0800

    edited order_creater : Added loop for product & q

--- eshop/order_create.php
+++ eshop/order_create.php
@@ -58,12 +58,6 @@
         $stmt = $con->prepare($q);
         $stmt->execute();
 
-        $stmt2 = $con->prepare($q);
-        $stmt2->execute();
-
-        $stmt3 = $con->prepare($q);
-        $stmt3->execute();
-
         $cus_username = "SELECT username FROM customers";
 
         $cu = $con->prepare($cus_username);
@@ -74,14 +68,8 @@
             // include database connection
             include 'config/database.php';
 
-            $q1 = "SELECT product_id, name, price FROM products WHERE name='" . $_POST['product1'] . "'";
-            $stmt = $con->prepare($q1);
-            $stmt->execute();
-            while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-
-                $p1 = $row['price'];
-            }
-
+            var_dump($_POST);
+            /*
             try {
                 // insert query
                 $query = "INSERT INTO order_details SET name=:product1, quantity=:quantity1, price=:price1";
@@ -90,7 +78,7 @@
                 $stmt = $con->prepare($query);
                 $product1 = $_POST['product1'];
                 $quantity1 = $_POST['quantity1'];
-                $price1 = $p1 * $_POST['quantity1'];
+                //$price1 = $p1 * $_POST['quantity1'];
                 //$product2 = $_POST['product2'];
                 //$quantity2 = $_POST['quantity2'];
                 // bind the parameters
@@ -109,22 +97,29 @@
             } catch (PDOException $exception) {
                 die('ERROR: ' . $exception->getMessage());
             }
-
+*/
             try {
                 // insert query
-                $query = "INSERT INTO order_summary SET username=:cus_username, total=:total, purchase_date=:purchase_date";
+                $query = "INSERT INTO order_summary SET username=:cus_username, purchase_date=:purchase_date";
                 // prepare query for execution
                 $stmt = $con->prepare($query);
                 $cus_username = $_POST['cus_username'];
                 // bind the parameters
                 $stmt->bindParam(':cus_username', $cus_username);
-                $total = $price1;
-                $stmt->bindParam(':total', $total);
                 $purchase_date = date('Y-m-d H:i:s'); // get the current date and time
                 $stmt->bindParam(':purchase_date', $purchase_date);
 
                 if ($stmt->execute()) {
-                    echo "<div class='alert alert-success'>Record was saved.</div>";
+                    $last_id = $con->lastInsertId();
+                    for ($count = 0; $count < 3; $count++) {
+                        $query2 = "INSERT INTO order_details SET order_id=:order_id, product_id=:product_id, quantity=:quantity";
+                        $stmt = $con->prepare($query2);
+                        $stmt->bindParam(':order_id', $last_id);
+                        $stmt->bindParam(':product_id', $_POST['product'][$count]);
+                        $stmt->bindParam(':quantity', $_POST['quantity'][$count]);
+                        $stmt->execute();
+                    }
+                    echo "<div class='alert alert-success'>Record was saved.Last inserted ID is: $last_id</div>";
                 } else {
                     echo "<div class='alert alert-danger'>Unable to save record.</div>";
                 }
@@ -144,7 +139,7 @@
                     <?php
                     echo "<td>";
                     echo '<select class="w-100 fs-4 rounded" id="" name="cus_username">';
-                    echo  '<option class="bg-white" selected>Select Your Username</option>';
+                    echo  '<option class="bg-white" disable selected value>Select Your Username</option>';
                     while ($row = $cu->fetch(PDO::FETCH_ASSOC)) {
                         extract($row);
                         echo "<option class='bg-white' value='" . $row['username'] . "'>" . $row['username'] . "</option>";
@@ -159,80 +154,33 @@
                 <?php
                 $quantity = 1;
 
-                echo "<tr>";
-                echo '<td>
-                       <select class="fs-4 rounded" name="product1">';
-                echo  '<option class="bg-white" selected>--Select--</option>';
+                $product_arrID = array();
+                $product_arrName = array();
                 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-                    extract($row);
-                    echo "<option class='bg-white' value='" . $row['name'] . "'>" . $row['name'] . "</option>";
+                    array_push($product_arrID, $row['product_id']);
+                    array_push($product_arrName, $row['name']);
                 }
-                echo "</select>";
-                echo '</td>';
-                echo "<td>";
-                echo '<select class="w-100 fs-4 rounded" name="quantity1" class="form-control">';
-                echo "<option class='bg-white' selected>Please Select Your Quantity</option>";
-                for ($quantity = 0; $quantity <= 5; $quantity++) {
-                    echo "<option value='$quantity'>$quantity</option>";
+                //print_r($product_arr);
+
+                for ($x = 0; $x <= 2; $x++) {
+                    echo "<tr>";
+                    echo '<td>
+                       <select class="fs-4 rounded" name="product[]">';
+                    echo  '<option disable selected value>--Select--</option>';
+                    for ($product_count = 0; $product_count < count($product_arrName); $product_count++) {
+                        echo  "<option value='" . $product_arrID[$product_count] . "'>" . $product_arrName[$product_count] . "</option>";
+                    }
+                    echo "</select>";
+                    echo '</td>';
+                    echo "<td>";
+                    echo '<select class="w-100 fs-4 rounded" name="quantity[]" class="form-control">';
+                    echo "<option class='bg-white' disable selected value>Please Select Your Quantity</option>";
+                    for ($quantity = 1; $quantity <= 5; $quantity++) {
+                        echo "<option value='$quantity'>$quantity</option>";
+                    }
+                    echo '</td>';
+                    echo "</tr>";
                 }
-                echo '</td>';
-                echo "</tr>";
-                ?>
-
-                <tr>
-                    <th>Products 2<span class="fw-light">(Optional)</span></th>
-                    <th>Quantity</th>
-                </tr>
-                <?php
-
-                $quantity = 0;
-
-                echo "<tr>";
-                echo '<td>
-                       <select class="fs-4 rounded" id="" name="product1">';
-                echo  '<option class="bg-white" selected>--Select--</option>';
-                while ($row2 = $stmt2->fetch(PDO::FETCH_ASSOC)) {
-                    extract($row2);
-                    echo "<option class='bg-white' value='" . $row2['name'] . "'>" . $row2['name'] . "</option>";
-                }
-                echo "</select>";
-                echo '</td>';
-                echo "<td>";
-                echo '<select class="w-100 fs-4 rounded" name="quantity1" class="form-control">';
-                echo "<option class='bg-white' selected>Please Select Your Quantity</option>";
-                for ($quantity = 0; $quantity <= 5; $quantity++) {
-                    echo "<option value='$quantity'>$quantity</option>";
-                }
-                echo '</td>';
-                echo "</tr>";
-
-                ?>
-                <tr>
-                    <th>Products 3<span class="fw-light">(Optional)</span></th>
-                    <th>Quantity</th>
-                </tr>
-                <?php
-                $quantity = 0;
-
-                echo "<tr>";
-                echo '<td>
-                       <select class="fs-4 rounded" id="" name="product1">';
-                echo  '<option class="bg-white" selected>--Select--</option>';
-                while ($row3 = $stmt3->fetch(PDO::FETCH_ASSOC)) {
-                    extract($row3);
-                    echo "<option class='bg-white' value='" . $row3['name'] . "'>" . $row3['name'] . "</option>";
-                }
-                echo "</select>";
-                echo '</td>';
-                echo "<td>";
-                echo '<select class="w-100 fs-4 rounded" name="quantity1" class="form-control">';
-                echo "<option class='bg-white' selected>Please Select Your Quantity</option>";
-                for ($quantity = 0; $quantity <= 5; $quantity++) {
-                    echo "<option value='$quantity'>$quantity</option>";
-                }
-                echo '</td>';
-                echo "</tr>";
-
                 ?>
 
                 <tr>
